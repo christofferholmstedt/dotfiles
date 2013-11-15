97a7e341f602c114f4d2b6b2ca4e93b391f4fc73
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-11-15 05:34:05 +0100

    Configuration script updated.

--- configuration.sh
+++ configuration.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 # Install vim
+# Already done...sudo apt-get install git vim
+# is the default installation command I use when I have a new machine
+
 # Configure vim
-# Install git
+# Assumption is made that this script is run from the dotfiles folder
+# which as been cloned directly into your home folder
+dotfiles_dir=$(pwd)
+echo $dotfiles_dir
+ln -sv ./dotfiles/vim/.vimrc ../.vimrc
+mkdir -v ../.vim
+cp -rv ./vim/requiredFiles/* ../.vim/
+cd ../.vim/bundle/
+
+git clone https://github.com/klen/python-mode
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/Lokaltog/vim-powerline.git
+
 # Configure git
-# user.name and user.email
+git config --global user.name "Christoffer Holmstedt"
+git config --global user.email "christoffer.holmstedt@gmail.com"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
